39813a449df3ada7dd32622a6d745dbca7d41419
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-21 17:07:40 +0300

    Update order.py

--- order.py
+++ order.py
@@ -43,21 +43,22 @@ class Order:
         self.cart.append(product)
         self.quantities.append(quantity)
 
+    def __iadd__(self, other: Product | tuple):
+        if isinstance(other, Product):
+            self.cart.append(other)
+            self.quantities.append(1)
+        elif isinstance(other, tuple):
+            self.cart.append(other[0])
+            self.quantities.append(other[1])
+        else:
+            raise TypeError()
+        return self
+
     def total_price(self):
         total = 0
         for i, item in enumerate(self.cart):
             total += item.price * self.quantities[i]
         return total
-
-    def __str__(self) -> str:
-        res = f'Customer: {self.customer}\n\n'
-        for i, item in enumerate(self.cart):
-            tmp = f'\t{item} UAH * {self.quantities[i]} = {self.quantities[i] * item.price}\n'
-            res += tmp
-
-        res += f'\nTotal price: {self.total_price()} UAH'
-
-        return res
 
     def __getitem__(self, item):
         
@@ -84,21 +85,32 @@ class Order:
         return len(self.cart)
 
     def __iter__(self):
-        return CartIter(self.cart)
+        return CartIter(self.cart, self.quantities)
+
+    def __str__(self) -> str:
+        res = f'Customer: {self.customer}\n\n'
+        for i, item in enumerate(self.cart):
+            tmp = f'\t{item} UAH * {self.quantities[i]} = {self.quantities[i] * item.price}\n'
+            res += tmp
+
+        res += f'\nTotal price: {self.total_price()} UAH'
+
+        return res
 
 class CartIter:
 
-    def __init__(self, wrapped):
-        self.wrapped = wrapped
+    def __init__(self, cart, quantity):
+        self.cart = cart
+        self.quantity = quantity
         self.index = 0
 
     def __iter__(self, ):
         return self
 
     def __next__(self):
-        if self.index < len(self.wrapped):
+        if self.index < len(self.cart):
             self.index += 1
-            return self.wrapped[self.index - 1]
+            return self.cart[self.index - 1], self.quantity[self.index - 1]
         raise StopIteration
 
 pr_1 = Product('Apple', 10)
@@ -107,22 +119,14 @@ pr_3 = Product('Apple_2', 10)
 pr_4 = Product('Banana_2', 20)
 
 cust_1 = Customer('A', 'B')
-
 order_1 = Order(cust_1)
-order_1.add_product(pr_1, 1)
-order_1.add_product(pr_2, 3)
-order_1.add_product(pr_3, 1)
-order_1.add_product(pr_4, 4)
+
+order_1 += (pr_1, 1)
+order_1 += (pr_2, 2)
+order_1 += (pr_3, 1)
+order_1 += (pr_4, 3)
 
 print(order_1)
-print(order_1[2])
-print()
 
-x = order_1[2:]
-for item in x:
-    print(item)
-
-print()
-
-for item in order_1:
-    print(item)
+for item, count in order_1:
+    print(item, count)
